9f00cf622d2a3de41271659c6900d900c2577429
DevJangSunWoo <sw6yyrdn@gmail.com>
2023-02-21 11:02:00 +0900

    2023-02-21[Golden] 1차원 배열 브론즈2 문제이해 필요 M은 공의 번호이다.

---
+++ jandi/src/com/sw/baekjoon/Practice_10810.java
@@ -0,0 +1,37 @@
+package com.sw.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Practice_10810 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+
+		String firstLine=br.readLine();
+		
+		String[] arr1=firstLine.trim().split(" ");
+		//System.out.println(arr1[0]);
+		
+		int N=Integer.parseInt(arr1[0]);
+		int M=Integer.parseInt(arr1[1]);
+		
+		int bucket[]=new int[N];
+		
+		for(int i=0;i<M;i++) {
+		 String testLine=br.readLine();
+		 String [] testArr=testLine.trim().split(" ");
+		 int numS=Integer.parseInt(testArr[0]);
+		 int numE=Integer.parseInt(testArr[1]);
+		 int numM=Integer.parseInt(testArr[2]);
+		 	for(int j=numS-1;j<numE;j++) {
+		 		bucket[j]=numM;
+		 	}	 
+		}
+		
+		for(int i=0;i<N;i++) {
+			System.out.print(bucket[i]+" ");
+		}		
+	}
+}
