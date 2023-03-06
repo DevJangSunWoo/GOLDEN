6a1380c40c8b37510be6bf61825afdeb2da56007
DevJangSunWoo <sw6yyrdn@gmail.com>
2023-03-06 13:57:08 +0900

    브론즈1 2609 유클리드 호제법  최대공약 최소공배수 재귀방식 유클리드 호제법

--- jandi/src/com/sw/baekjoon/Practice_2609.java
+++ jandi/src/com/sw/baekjoon/Practice_2609.java
@@ -6,34 +6,30 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Practice_2609 {
-		// 유클리드 호제법
+	// 유클리드 호제법
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub     최대공약 최소공배수 재귀방식 유클리드 호제법
+		// TODO Auto-generated method stub 최대공약 최소공배수 재귀방식 유클리드 호제법
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());          
-		int a= Integer.parseInt(st.nextToken());  //24
-		int b= Integer.parseInt(st.nextToken());  //18
-		int d = gcd(a, b);	// 최대공약수
-		 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken()); // 24
+		int b = Integer.parseInt(st.nextToken()); // 18
+		int d = gcd(a, b); // 최대공약수
+
 		System.out.println(d);
 		System.out.println(a * b / d); // 최소공배수
-		
+
 	}
 
 	// 최대공약수 재귀 방식
-		public static int gcd(int a, int b) {
-			if (b == 0)
-				return a;
-	            
-			// GCD(a, b) = GCD(b, r)이므로 (r = a % b)
-			return gcd(b, a % b);   //(18,6)  -> (6,0) 이상태에서 위 이프문에 걸리면  결국에 반환되는 값은 a이다. 
-		}
-		
-		
-		
+	public static int gcd(int a, int b) {
+		if (b == 0)
+			return a;
+
+		// GCD(a, b) = GCD(b, r)이므로 (r = a % b)
+		return gcd(b, a % b); // (18,6) -> (6,0) 이상태에서 위 이프문에 걸리면 결국에 반환되는 값은 a이다.
+	}
+
 }
-
-
 //		int first=x%y;	 // 나머지	
 //		int second=y%first;  //나머지2
 //		//System.out.println(first+" "+second);
