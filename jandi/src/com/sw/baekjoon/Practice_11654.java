2c5a0baf4d833ed0056ae09a495d49a3f47bf89b
DevJangSunWoo <sw6yyrdn@gmail.com>
2023-03-10 19:00:01 +0900

    아스키 코드

---
+++ jandi/src/com/sw/baekjoon/Practice_11654.java
@@ -0,0 +1,14 @@
+package com.sw.baekjoon;
+
+import java.io.IOException;
+
+public class Practice_11654 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub  아스키코드
+		int a = System.in.read();
+		System.out.print(a);
+ 
+	}
+
+}
